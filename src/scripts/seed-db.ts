bddb30e1435397dd6231d709f7bafc8a0bc6a300
AmitOfir4 <amit.ofir4@gmail.com>
2026-04-17 12:42:05 +0300

    update

--- src/scripts/seed-db.ts
+++ src/scripts/seed-db.ts
@@ -2,6 +2,10 @@ import 'dotenv/config';
 import { seedDatabase } from '../db/seed-db';
 import { createPgPool } from '../db/pg';
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in the environment variables');
+}
+
 const pool = createPgPool({ connectionString: process.env.DATABASE_URL });
 
 seedDatabase(pool)
