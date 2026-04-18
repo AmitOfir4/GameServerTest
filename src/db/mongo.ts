95a91a771b3d9ba12d8f5ebd87e651118afcef34
AmitOfir4 <amit.ofir4@gmail.com>
2026-04-18 17:34:35 +0300

    fix mongo connection

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -7,7 +7,7 @@ export interface MongoConfig {
 
 export async function createMongoDb(config: MongoConfig): Promise<{ client: MongoClient; db: Db }> {
   const url = new URL(config.connectionString);
-  if (!url.searchParams.has("directConnection")) {
+  if (url.protocol !== "mongodb+srv:" && !url.searchParams.has("directConnection")) {
     url.searchParams.set("directConnection", "true");
   }
   const client = new MongoClient(url.toString());
